8c48e8f7f7f881c7053f5f47736bba193652cf09
biface <fabrice.moutte.wn@gmail.com>
2025-11-24 18:16:57 +0100

    tests:WIP  - exception coverage  - modification of the tox.ini file to optimize CPU usage (removal of coverage reports in tests, to be added later)  - addition of a Python version for local testing

--- tests/01_core/test_00_exceptions.py
+++ tests/01_core/test_00_exceptions.py
@@ -1,19 +1,85 @@
 import re
 
-from sds.core.exceptions import DataStructureError, EmptyStructureError
+import pytest
+
+from sds.core.exceptions import (
+    DataStructureError,
+    EmptyStructureError,
+    FullStructureError,
+    IndexError,
+    InvalidOperationError,
+)
 
 
-def test_data_structure_exception():
-    e = DataStructureError()
-    assert isinstance(e, DataStructureError)
-    assert isinstance(e, Exception)
+@pytest.mark.parametrize(
+    "class_name, class_hierarchy, default_message",
+    [
+        (DataStructureError, [Exception], None),
+        (
+            EmptyStructureError,
+            [Exception, DataStructureError],
+            "Cannot perform operation on empty structure",
+        ),
+        (
+            FullStructureError,
+            [Exception, DataStructureError],
+            "Cannot add to full structure",
+        ),
+        (InvalidOperationError, [Exception, DataStructureError], "Invalid operation"),
+        (IndexError, [Exception, DataStructureError], "Index out of range"),
+    ],
+)
+def test_data_structure_exception(class_name, class_hierarchy, default_message):
+    e = class_name()
+
+    for class_category in class_hierarchy:
+        assert isinstance(e, class_category)
+
+    if default_message is not None:
+        assert re.escape(e.message) == re.escape(default_message)
 
 
-def test_empty_structure_exception():
-    e = EmptyStructureError()
-    assert isinstance(e, EmptyStructureError)
-    assert isinstance(e, DataStructureError)
-    assert isinstance(e, Exception)
-    assert re.escape(e.message) == re.escape(
-        "Cannot perform operation on empty structure"
-    )
+@pytest.mark.parametrize(
+    "class_name, class_hierarchy, default_message, other_message",
+    [
+        (DataStructureError, [Exception], None, None),
+        (
+            EmptyStructureError,
+            [Exception, DataStructureError],
+            "Cannot perform operation on empty structure",
+            "Impossible de réaliser cette action",
+        ),
+        (
+            FullStructureError,
+            [Exception, DataStructureError],
+            "Cannot add to full structure",
+            "Impossible d'ajout à la structure",
+        ),
+        (
+            InvalidOperationError,
+            [Exception, DataStructureError],
+            "Invalid operation",
+            "Operation interdite",
+        ),
+        (
+            IndexError,
+            [Exception, DataStructureError],
+            "Index out of range",
+            "N'est pas dans le segment d'index",
+        ),
+    ],
+)
+def test_data_structure_exception_with_message(
+    class_name, class_hierarchy, default_message, other_message
+):
+    if other_message is not None:
+        e = class_name(other_message)
+    else:
+        e = class_name()
+
+    for class_category in class_hierarchy:
+        assert isinstance(e, class_category)
+
+    if default_message is not None:
+        assert re.escape(e.message) != re.escape(default_message)
+        assert re.escape(e.message) == re.escape(other_message)
